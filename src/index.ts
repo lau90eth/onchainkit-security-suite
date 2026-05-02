ba068b38e43037d209066e038bb720ba2921605b
lau90eth <lau90eth@users.noreply.github.com>
2026-05-02 17:37:45 +0200

    feat: add main index exports

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// Hooks
+export { useNetworkGuard } from './hooks/useNetworkGuard';
+export { useTransactionHistory } from './hooks/useTransactionHistory';
+export { useContractVerification } from './hooks/useContractVerification';
+export { useTokenAllowance } from './hooks/useTokenAllowance';
+export { useRevokeAllowance } from './hooks/useRevokeAllowance';
+export { useGasEstimator } from './hooks/useGasEstimator';
+
+// Components
+export { SecurityCheck } from './components/SecurityCheck';
+
+// Utils
+export { formatFee } from './utils/formatFee';
+export { shortenAddress } from './utils/shortenAddress';
+export { isInfiniteApproval } from './utils/isInfiniteApproval';
