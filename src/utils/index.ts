b2a1a18d9faf007d0664155ba0b77b69354fef06
lau90eth <lau90eth@users.noreply.github.com>
2026-05-02 17:42:14 +0200

    feat: add utils barrel export

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export { formatFee } from './formatFee';
+export { formatGasPrice } from './formatGasPrice';
+export { shortenAddress } from './shortenAddress';
+export { isInfiniteApproval } from './isInfiniteApproval';
+export { isContractAddress } from './isContractAddress';
+export { getRiskColor } from './getRiskColor';
+export { getRiskLabel } from './getRiskLabel';
